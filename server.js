52a3ffea961b567aef640c170d492fdd3bf5c3c9
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-02 10:33:13 +0100

    deleted the node modules folder

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from 'express'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 import notFoundMiddleware from './middleware/not-found.js'
+import dotenv from 'dotenv'
+dotenv.config
 const app = express()
 
 app.get('/', (req, res) => {
+  throw new Error('error')
   res.send('welcome')
 })
 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 6000
 
-app.listen(port, () => {
+app.listen(port, (req, res) => {
   console.log(`server is running on port ${port}`)
 })

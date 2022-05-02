358a6631e258c38a73edbe314f3bab03afe145be
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-02 10:48:44 +0100

    Added the errorHandlerMiddleware and also connected to a local db. Upon deployment the i'll switch to an online db

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import connectDB from './db/connect.js'
+
+// Middlewares
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import notFoundMiddleware from './middleware/not-found.js'
 import dotenv from 'dotenv'
-dotenv.config
+dotenv.config()
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -15,6 +19,15 @@ app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 6000
 
-app.listen(port, (req, res) => {
-  console.log(`server is running on port ${port}`)
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, (req, res) => {
+      console.log(`server is running on port ${port}`)
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start()

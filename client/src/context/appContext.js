bcd696a34350b4ff08f5335166e6d1986cbaf499
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-08 14:45:26 +0100

    Edit job done

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -21,6 +21,9 @@ import {
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
   DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_SUCCESS,
+  EDIT_JOB_ERROR,
 } from './action'
 
 const token = localStorage.getItem('token')
@@ -220,8 +223,30 @@ const AppProvider = ({ children }) => {
       logoutUser()
     }
   }
-  const editJob = () => {
-    console.log('edit job')
+  const editJob = async () => {
+    dispatch({ type: EDIT_JOB_BEGIN })
+    try {
+      const { position, company, jobLocation, jobType, status } = state
+
+      await authFetch.patch(`/jobs/${state.editJobId}`, {
+        company,
+        position,
+        jobLocation,
+        jobType,
+        status,
+      })
+      dispatch({
+        type: EDIT_JOB_SUCCESS,
+      })
+      dispatch({ type: CLEAR_VALUES })
+    } catch (error) {
+      if (error.response.status === 401) return
+      dispatch({
+        type: EDIT_JOB_ERROR,
+        payload: { msg: error.response.data.msg },
+      })
+    }
+    clearAlert()
   }
   return (
     <AppContext.Provider

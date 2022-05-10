deba30ee0e00114c3385c447923be77eabdabb97
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-10 11:40:32 +0100

    pagination done server

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -26,6 +26,7 @@ import {
   EDIT_JOB_ERROR,
   SHOW_STATS_BEGIN,
   SHOW_STATS_SUCCESS,
+  CLEAR_FILTERS,
 } from './action'
 
 const token = localStorage.getItem('token')
@@ -57,6 +58,11 @@ const initialState = {
   page: 1,
   stats: {},
   monthlyApplications: [],
+  search: '',
+  searchStatus: 'all',
+  searchType: 'all',
+  sort: 'latest',
+  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
 }
 
 const AppContext = React.createContext()
@@ -196,7 +202,11 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
   const getJobs = async () => {
-    let url = `/jobs`
+    const { search, searchStatus, searchType, sort } = state
+    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}`
+    if (search) {
+      url = url + `&search=${search}`
+    }
 
     dispatch({ type: GET_JOBS_BEGIN })
     try {
@@ -271,6 +281,9 @@ const AppProvider = ({ children }) => {
 
     clearAlert()
   }
+  const clearFilters = () => {
+    dispatch({ type: CLEAR_FILTERS })
+  }
   return (
     <AppContext.Provider
       value={{
@@ -289,6 +302,7 @@ const AppProvider = ({ children }) => {
         deleteJob,
         editJob,
         showStats,
+        clearFilters,
       }}
     >
       {children}

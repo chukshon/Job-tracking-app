9c0eb1fcf1d3c7a0308af9396220305d5e2c09bb
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-10 11:03:57 +0100

    Search done server

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -7,19 +7,45 @@ import moment from 'moment'
 
 const createJob = async (req, res) => {
   const { position, company } = req.body
-
   if (!position || !company) {
     throw new badRequestError('Please Provide All Values')
   }
-
   req.body.createdBy = req.user.userId
-
   const job = await Job.create(req.body)
   res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId })
+  const { search, status, jobType, sort } = req.query
+
+  const queryObject = {
+    createdBy: req.user.userId,
+  }
+  if (status !== 'all') {
+    queryObject.status = status
+  }
+  if (jobType !== 'all') {
+    queryObject.jobType = jobType
+  }
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' }
+  }
+  let result = Job.find(queryObject)
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt')
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt')
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position')
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position')
+  }
+  const jobs = await result
+
   res
     .status(StatusCodes.OK)
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
